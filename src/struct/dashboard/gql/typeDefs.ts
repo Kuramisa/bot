537fe042d90caefae09175da8e90dca56d750ffe
Mateie <kurama@mateie.com>
2022-10-14 16:43:57 -0700

    updated some typedefs for graphql

---
+++ src/struct/dashboard/gql/typeDefs.ts
@@ -0,0 +1,66 @@
+import { gql } from "apollo-server-core";
+
+export default gql`
+    scalar Client
+
+    scalar Guild
+    scalar Role
+    scalar Emoji
+
+    scalar Channel
+    scalar Message
+
+    scalar User
+    scalar Member
+
+    scalar Object
+
+    scalar Ticket
+
+    type Query {
+        client: Client!
+        clientUser: User!
+
+        guild(guildId: String!, fetchDb: Boolean): Guild!
+        guilds(fetchDb: Boolean): [Guild]!
+
+        role(guildId: String!, roleId: String!): Role!
+        roles(guildId: String!): [Role]!
+
+        channel(guildId: String!, channelId: String!): Channel!
+        channels(guildId: String!): [Channel]!
+
+        message(
+            guildId: String!
+            channelId: String!
+            messageId: String!
+        ): Message!
+        messages(guildId: String!, channelId: String!): [Message]!
+
+        emoji(guildId: String!, emojiId: String!): Emoji!
+        emojis(guildId: String!): [Emoji]!
+
+        tickets(guildId: String!): [Ticket]!
+        ticket(guildId: String!, ticketId: String!): Ticket!
+        ticketTranscript(guildId: String!, ticketId: String!): Ticket
+        memberTickets(guildId: String!, memberId: String!): [Ticket]!
+
+        user(userId: String!, fetchDb: Boolean): User!
+        userCard(userId: String!): User!
+        users(fetchDb: Boolean): [User]!
+        userGuilds(auth: String!, fetchDb: Boolean): [Guild]!
+
+        member(guildId: String!, memberId: String!, fetchDb: Boolean): Member!
+        members(guildId: String!, fetchDb: Boolean): [Member]!
+    }
+
+    type Mutation {
+        login(code: String!): String!
+        authUser(auth: String!): User!
+
+        linkMinecraft(username: String!, code: String!): String!
+        unlinkMinecraft(username: String!): String!
+
+        music(username: String!, action: String!, query: String): String!
+    }
+`;
