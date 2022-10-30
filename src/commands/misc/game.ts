66ccc5bb80e50c5a9f44b6b772d18d6fdf6aca65
Mateie <mateie@mateie.com>
2022-10-30 16:04:46 -0700

    fixed case sensitivity isue

--- src/commands/misc/game.ts
+++ src/commands/misc/game.ts
@@ -150,7 +150,7 @@ export class GameCommand extends Command {
 
         const forObj = gameToUse.toLowerCase();
 
-        if (!db.games.list.includes(gameToUse))
+        if (!db.games.list.includes(gameToUse.toLowerCase()))
             return interaction.reply({
                 content: "Game is not supported currently",
                 ephemeral: true
