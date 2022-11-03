accd9200f8af77b75323e8c2eb1199a111233b9c
Mateie <mateie@mateie.com>
2022-11-03 14:25:06 -0700

    enabled https

--- src/struct/dashboard/index.ts
+++ src/struct/dashboard/index.ts
@@ -7,6 +7,7 @@ import { Container } from "@sapphire/pieces";
 import express from "express";
 import helmet from "helmet";
 import http from "http";
+import https from "https";
 import cors from "cors";
 import bodyParser from "body-parser";
 
@@ -18,7 +19,19 @@ import typeDefs from "./gql/typeDefs";
 const app = express();
 app.use(helmet());
 
-const httpServer = http.createServer(app);
+const configurations = {
+    // Note: You may need sudo to run on port 443
+    production: { ssl: true, port: 443, hostname: "api.kuramisa.com" },
+    development: { ssl: false, port: 4000, hostname: "localhost" }
+};
+
+const environment = process.env.NODE_ENV || "production";
+const config = configurations[environment as keyof typeof configurations];
+
+let httpServer = http.createServer(app);
+if (config.ssl) {
+    httpServer = https.createServer(app);
+}
 
 export default class Dashboard extends ApolloServer {
     private readonly container: Container;
