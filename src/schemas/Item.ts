3e13e75d07c57bc11be36406e5bd1ee10627ee37
Mateie <mateie@mateie.com>
2022-11-06 09:36:38 -0800

    added an item schema

---
+++ src/schemas/Item.ts
@@ -0,0 +1,34 @@
+import { Document, model, Schema, HydratedDocument, Types } from "mongoose";
+
+export interface IItem extends HydratedDocument<any> {
+    id: string;
+    name: string;
+    description: string;
+    usage: string;
+    price?: number;
+}
+
+export const Item: Schema = new Schema<IItem>({
+    id: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    usage: {
+        type: String,
+        required: true
+    },
+    price: Number
+});
+
+export type TItem = Document<unknown, any, IItem> &
+    IItem & { _id: Types.ObjectId };
+
+export default model<IItem>("items", Item);
