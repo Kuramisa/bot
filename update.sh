a85dc5d85c4e33658c3d2d6550cd7ec80d3aae30
Mateie <kurama@mateie.com>
2022-11-11 14:18:55 -0800

    updated the script

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 clear
-git pull bot master
-yarn build
-sudo pm2 restart all
+echo "Pulling updates from Github"
+git pull bot master > /dev/null
+echo "Updating Packages"
+yarn > /dev/null
+echo "Building the bot"
+yarn build > /dev/null
