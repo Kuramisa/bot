399abcc4b6e45a3f2f4839f866d6c544c327489f
Mateie <kurama@mateie.com>
2022-11-20 15:18:55 -0800

    added new types

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -10,6 +10,21 @@ export interface ValorantAccount {
     rank: any;
 }
 
+export type IWarn = {
+    id: string;
+    guildId: string;
+    by: string;
+    reason: string;
+};
+
+export type IReport = {
+    id: string;
+    guildId: string;
+    by: string;
+    message?: { id: string; content: string };
+    reason: string;
+};
+
 export type CardType = "buffer" | "attachment";
 
 // Shinobi Types
