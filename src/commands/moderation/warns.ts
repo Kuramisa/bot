e3e12d70eace589a9eb28565a4206b01a4a05ed6
Mateie <kurama@mateie.com>
2022-12-01 15:55:30 -0800

    Refactored code and fixed typos

--- src/commands/moderation/warns.ts
+++ src/commands/moderation/warns.ts
@@ -35,7 +35,7 @@ export class WarnsCommand extends Command {
                         .addUserOption((option) =>
                             option
                                 .setName("member")
-                                .setDescription("Member of check warns of")
+                                .setDescription("Member of remove warns of")
                                 .setRequired(true)
                         )
                 )
@@ -54,35 +54,33 @@ export class WarnsCommand extends Command {
     }
 
     async chatInputRun(interaction: ChatInputCommandInteraction<"cached">) {
-        const { moderation, util } = this.container;
+        const { client, moderation, util } = this.container;
 
-        const { options, guild } = interaction;
+        const { options } = interaction;
+
+        const member = options.getMember("member");
+
+        if (!member) return;
+
+        if (member.user.bot)
+            return interaction.reply({
+                content: `${member} is a bot`,
+                ephemeral: true,
+            });
+
+        const warns = await moderation.warns.get(member);
+
+        if (!warns || warns.length < 1)
+            return interaction.reply({
+                content: `${member} has no warns`,
+                ephemeral: true,
+            });
 
         switch (options.getSubcommand()) {
             case "view": {
-                const member = options.getMember("member");
-
-                if (!member) return;
-
-                if (member.user.bot)
-                    return interaction.reply({
-                        content: `${member} is a bot`,
-                        ephemeral: true,
-                    });
-
-                const warns = await moderation.warns.get(member);
-
-                if (!warns || warns.length < 1)
-                    return interaction.reply({
-                        content: `${member} has no warns`,
-                        ephemeral: true,
-                    });
-
                 const warnMap = warns.map(
                     (warn) =>
-                        `**Warned by**: ${guild.members.cache.get(
-                            warn.by
-                        )}\n**Reason**: ${warn.reason}`
+                        `**Warned by**: <@${warn.by}>\n**Reason**: ${warn.reason}`
                 );
 
                 return util.pagination.default(
@@ -93,30 +91,16 @@ export class WarnsCommand extends Command {
                 );
             }
             case "remove": {
-                const member = options.getMember("member");
-
-                if (!member) return;
-
-                if (member.user.bot)
-                    return interaction.reply({
-                        content: `${member} is a bot`,
-                        ephemeral: true,
-                    });
-
-                const warns = await moderation.warns.get(member);
-
-                if (!warns || warns.length < 1)
-                    return interaction.reply({
-                        content: `${member} has no warns`,
-                        ephemeral: true,
-                    });
-
-                const opts = warns.map((warn) => ({
-                    label: `Reason: ${warn.reason} - Warned by: ${
-                        guild.members.cache.get(warn.by)?.user.username
-                    }`,
-                    value: `${warn.id}`,
-                }));
+                const opts = await Promise.all(
+                    warns.map(async (warn) => ({
+                        label: `Reason: ${warn.reason} - Warned by: ${
+                            (
+                                await client.users.fetch(warn.by)
+                            )?.username
+                        }`,
+                        value: warn.id,
+                    }))
+                );
 
                 const embed = util
                     .embed()
@@ -150,8 +134,7 @@ export class WarnsCommand extends Command {
 
                 const warnsRemoved = [];
 
-                for (let i = 0; i < values.length; i++) {
-                    const value = values[i];
+                for (const value of values) {
                     warnsRemoved.push(
                         (await moderation.warns.get(member))?.find(
                             (warn) => warn.id === value
@@ -171,13 +154,7 @@ export class WarnsCommand extends Command {
                                 warnsRemoved
                                     .map(
                                         (warn) =>
-                                            `Reason: ${
-                                                warn?.reason
-                                            } - Warned by: ${
-                                                guild.members.cache.get(
-                                                    warn?.by as string
-                                                )?.user.username
-                                            }`
+                                            `Reason: ${warn?.reason} - Warned by: <@${warn?.by}>`
                                     )
                                     .join("\n")
                             ),
@@ -187,24 +164,6 @@ export class WarnsCommand extends Command {
                 break;
             }
             case "clear": {
-                const member = options.getMember("member");
-
-                if (!member) return;
-
-                if (member.user.bot)
-                    return interaction.reply({
-                        content: `${member} is a bot`,
-                        ephemeral: true,
-                    });
-
-                const warns = await moderation.warns.get(member);
-
-                if (!warns || warns.length < 1)
-                    return interaction.reply({
-                        content: `${member} has no warns`,
-                        ephemeral: true,
-                    });
-
                 await moderation.warns.clear(member);
                 await interaction.reply({
                     content: `Cleared warns of ${member}`,
