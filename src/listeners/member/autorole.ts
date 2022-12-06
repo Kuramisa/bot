360696e3d2169fef2e340d4f13571a7fdf86fdc6
Mateie <kurama@mateie.com>
2022-12-05 18:19:41 -0800

    added an autorole listener

---
+++ src/listeners/member/autorole.ts
@@ -0,0 +1,26 @@
+import { Listener } from "@sapphire/framework";
+import { GuildMember } from "discord.js";
+
+export class AutoRoleMemberAdd extends Listener {
+    constructor(ctx: Listener.Context, opts: Listener.Options) {
+        super(ctx, {
+            ...opts,
+            name: "Autorole listener",
+            event: "guildMemberAdd",
+        });
+    }
+
+    async run(member: GuildMember) {
+        const { database } = this.container;
+        const { guild } = member;
+
+        const db = await database.guilds.get(guild);
+        if (!db) return;
+
+        if (db.autorole.length === 0) return;
+
+        for (const role of db.autorole) {
+            await member.roles.add(role);
+        }
+    }
+}
